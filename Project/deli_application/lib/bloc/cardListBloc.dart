f8bad029fca00cb57afe64739a90b32c51687bd1
Mrodriguez245 <60236590+Mrodriguez245@users.noreply.github.com>
2020-05-03 22:43:47 -0700

    Project as of 5/3

--- Project/deli_application/lib/bloc/cardListBloc.dart
+++ Project/deli_application/lib/bloc/cardListBloc.dart
@@ -1,30 +1,28 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:deliapplication/bloc/provider.dart';
+import 'package:deliapplication/model/deliitem.dart';
 import 'package:rxdart/rxdart.dart';
 
-class CounterBloc extends BlocBase{
+class CartListBloc extends BlocBase{
+  // ignore: non_constant_identifier_names
+  CartListBloc();
 
-  Observable<int> counter; // observable (Stream)
+  var _listController = BehaviorSubject<List<DeliItem>>.seeded([]);
 
-  CounterBloc() {
-    counter = Observable.merge([ //merges the both streams
-      _increment,
-      _decrement,
-    ]).startWith(0) //starts with the value 0(the initial data)
-        .scan((acc, curr, i) => acc + curr, 0 /* inital value: 0 */) // scans the old(acc) and the current(curr) value, and sum them
-        .asBroadcastStream(); //turns the stream into a Broadcast straem(it can be listened to more than once)
+  CartProvider provider = CartProvider();
+
+  Stream<List<DeliItem>> get listStream => _listController.stream;
+
+  Sink<List<DeliItem>> get listSink => _listController.sink;
+
+  addToList(DeliItem deliItem) {
+    listSink.add(provider.addToList(deliItem));
   }
-
-  final _increment = new BehaviorSubject<int>(); //the BehaviorSubject gets the last value
-  final _decrement = new BehaviorSubject<int>();
-
-  void increment() => _increment.add(1); //method to increment
-  void decrement() => _decrement.add(-1);//method to decrement
-
-
-  @override
-  void dispose() {// will be called automatically
-    _increment.close();
-    _decrement.close();
+  removeFromList(DeliItem deliItem) {
+    listSink.add(provider.removeFromList(deliItem));
+  }  @override
+  void dispose() {
+    _listController.close();
+    super.dispose();
   }
-
 }
