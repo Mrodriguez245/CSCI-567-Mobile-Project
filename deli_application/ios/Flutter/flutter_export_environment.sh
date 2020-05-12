aff6713077d504c8741ffb31163b9f8d37df3c19
Cody Evans <1123code@gmail.com>
2020-05-11 18:47:25 -0700

    Lot of build files changed?

---
+++ deli_application/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=B:\flutter"
+export "FLUTTER_APPLICATION_PATH=B:\Desktop\School\Spring 2020\Mobile Project\CSCI-567-Mobile-Project\Project\deli_application"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=B:\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
